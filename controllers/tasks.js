9b54bbd7fbb52cc8c8de6823cc990afae61881f2
Meet587 <meet.rakholiya.hp@gmail.com>
2023-12-12 23:20:58 +0530

    a

--- controllers/tasks.js
+++ controllers/tasks.js
@@ -1,12 +1,32 @@
 const Task = require("../models/Task");
-const getAllTask = (req, res) => {
-  res.send(`get all task`);
+const getAllTask = async (req, res) => {
+  try {
+    const task = await Task.find({});
+    res.status(200).json({ task: task });
+  } catch (error) {
+    res.status(500).json({ msg: error });
+  }
+  // res.send(`get all task`);
 };
 const createTask = async (req, res) => {
-  const task = await Task.create(req.body);
-  res.status(201).json({ task });
+  try {
+    const task = await Task.create(req.body);
+    res.status(201).json({ task });
+  } catch (error) {
+    res.status(500).json({ msg: error });
+  }
 };
-const getTask = (req, res) => {
+const getTask = async (req, res) => {
+  try {
+    const { id: taskID } = req.params;
+    const task = await Task.findOne({ _id: taskID });
+    if (!task) {
+      return res.status(404).json({ msg: `no task found with id ${taskID}` });
+    }
+    res.status(200).json({ task });
+  } catch (error) {
+    res.status(500).json({ msg: error });
+  }
   res.json({ id: req.params.id });
 };
 const updateTask = (req, res) => {
